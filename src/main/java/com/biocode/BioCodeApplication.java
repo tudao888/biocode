6214315be6302b2dab2858c33bf414a6079731bd
tudao888 <daotu2106@gmail.com>
2023-03-18 10:30:34 +0700

    done

---
+++ src/main/java/com/biocode/BioCodeApplication.java
@@ -0,0 +1,13 @@
+package com.biocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BioCodeApplication.class, args);
+    }
+
+}
